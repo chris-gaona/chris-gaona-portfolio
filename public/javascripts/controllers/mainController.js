7e30c90356e4bb7f4b92c1aabc60865ce50579b0
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-27 21:53:37 -0700

    Adds mixitup plugin as a directive

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -5,6 +5,31 @@
     var vm = this;
 
     vm.hello = 'My Portfolio';
+
+    //mixitup
+    vm.categories = ['Soft', 'Elements'];
+
+    vm.drawings = [{
+        name: 'Water',
+        category: 'Elements',
+        value: '2'
+    }, {
+        name: 'Fire',
+        category: 'Elements',
+        value: '1'
+    }, {
+        name: 'Air',
+        category: 'Elements',
+        value: '4'
+    }, {
+        name: 'Coton',
+        category: 'Soft',
+        value: '3'
+    }, {
+        name: 'Whool',
+        category: 'Soft',
+        value: '5'
+    }];
   }
 
   angular.module('app')
