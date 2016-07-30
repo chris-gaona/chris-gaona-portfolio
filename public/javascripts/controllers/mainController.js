66e23435a66ccab9bc606d147455a99cae3d6014
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-29 21:41:30 -0700

    Adds hamburger menu that rotates to X

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -13,27 +13,32 @@
         name: 'Random Quote Generator',
         category: 'Elements',
         image: '../images/random-quote-generator.png',
-        updated_at: 'July 23, 2016'
+        updated_at: 'July 23, 2016',
+        link: 'http://amazon.com'
     }, {
         name: 'Pagination Filter',
         category: 'Elements',
         image: 'images/pagination-filter.png',
-        updated_at: 'September 5, 2015'
+        updated_at: 'September 5, 2015',
+        link: ''
     }, {
         name: 'Interactive Form',
         category: 'Elements',
         image: 'images/interactive-form.png',
-        updated_at: 'March 5, 2016'
+        updated_at: 'March 5, 2016',
+        link: ''
     }, {
         name: 'Tic Tac Toe Game',
         category: 'Soft',
         image: 'images/tic-tac-toe.png',
-        updated_at: 'April 10, 2016'
+        updated_at: 'April 10, 2016',
+        link: ''
     }, {
         name: 'Movie Search',
         category: 'Soft',
         image: 'images/movie-search.png',
-        updated_at: 'April 28, 2016'
+        updated_at: 'April 28, 2016',
+        link: ''
     }];
   }
 
