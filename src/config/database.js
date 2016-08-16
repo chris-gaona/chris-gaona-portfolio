f33966325331408a9f981907cd9f2c4c5d1859cb
Chris Gaona <thebeloved88@sbcglobal.net>
2016-08-16 07:45:24 -0700

    Removes production database for now

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,12 @@
 'use strict';
 
 var config = {};
-var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
-var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
+// var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
+// var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
 
 config.mongoURI = {
   development: 'mongodb://localhost/portfolio',
-  test: 'mongodb://localhost/portfolio-test',
-  production: 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@jello.modulusmongo.net:27017/hibIdi9s'
+  test: 'mongodb://localhost/portfolio-test'
 };
 
 module.exports = config;
