670970126b8a21fa055f3fd2e6c10ebe4f51dc91
Chris Gaona <thebeloved88@sbcglobal.net>
2016-08-16 09:16:08 -0700

    Removes test script from build script; adds modulus database

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,13 @@
 'use strict';
 
 var config = {};
-// var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
-// var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
+var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
+var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
 
 config.mongoURI = {
   development: 'mongodb://localhost/portfolio',
-  test: 'mongodb://localhost/portfolio-test'
+  test: 'mongodb://localhost/portfolio-test',
+  production: 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@jello.modulusmongo.net:27017/hibIdi9s'
 };
 
 module.exports = config;
