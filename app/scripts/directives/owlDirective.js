ea60a39f00b0b69d41541d82b30d6b8d88cec439
Chris Gaona <thebeloved88@sbcglobal.net>
2016-08-19 13:55:43 -0700

    Fixes owl carousel bug when using finger to navigate

--- app/scripts/directives/owlDirective.js
+++ app/scripts/directives/owlDirective.js
@@ -6,10 +6,14 @@ function owlDirective () {
   return {
     // restrict to element only
     restrict: 'E',
+    transclude: false,
     link: function (scope, element) {
       var options = scope.$eval($(element).attr('data-options'));
 
-      $(element).owlCarousel(options);
+      // fixes issue with owl carousel showing {{}} when finger scrolling from github section to treehouse section
+      setInterval(function(){
+        $(element).owlCarousel(options);
+      }, 1000);
     },
     // adds template url
     templateUrl: '../templates/owl-carousel.html'
