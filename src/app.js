3b63b1d19abf721cd14bc3e888e38802a9dcbff7
Chris Gaona <thebeloved88@sbcglobal.net>
2016-08-19 08:04:40 -0700

    Fixes css issues in firefox browser; Adds icon to browser

--- src/app.js
+++ src/app.js
@@ -36,12 +36,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'images', 'logo.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', express.static('dist/public'));
 app.use('/api', routes.project);
 app.use('/api', routes.accomplishments);
