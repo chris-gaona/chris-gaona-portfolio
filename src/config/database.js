b5faadb4e77be7fdd497f5fa65f990c862b52e7d
Chris Gaona <thebeloved88@sbcglobal.net>
2016-08-30 16:25:58 -0700

    Adds Heroku config vars for test db; now runs lint & mocha tests on Heroku build

--- src/config/database.js
+++ src/config/database.js
@@ -6,10 +6,19 @@
 var config = {};
 var MODULUS_USERNAME = process.env.MODULUS_USERNAME;
 var MODULUS_PASSWORD = process.env.MODULUS_PASSWORD;
+var MODULUS_TEST_USERNAME = process.env.MODULUS_TEST_USERNAME;
+var MODULUS_TEST_PASSWORD = process.env.MODULUS_TEST_PASSWORD;
+var testDB;
+
+if (MODULUS_TEST_USERNAME !== undefined && MODULUS_TEST_PASSWORD !== undefined) {
+  testDB = 'mongodb://' + MODULUS_TEST_USERNAME + ':' + MODULUS_TEST_PASSWORD + '@jello.modulusmongo.net:27017/xabeGi8m';
+} else {
+  testDB = 'mongodb://localhost/portfolio-test';
+}
 
 config.mongoURI = {
   development: 'mongodb://localhost/portfolio',
-  test: 'mongodb://localhost/portfolio-test',
+  test: testDB,
   production: 'mongodb://' + MODULUS_USERNAME + ':' + MODULUS_PASSWORD + '@jello.modulusmongo.net:27017/hibIdi9s'
 };
 
