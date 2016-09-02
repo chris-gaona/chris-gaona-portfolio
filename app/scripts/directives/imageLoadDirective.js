374fd26de2543fd0d3604e684484182ad0765788
Chris Gaona <thebeloved88@sbcglobal.net>
2016-09-02 14:00:56 -0700

    Improves the image loader icon before image is fully loaded

--- app/scripts/directives/imageLoadDirective.js
+++ app/scripts/directives/imageLoadDirective.js
@@ -6,12 +6,16 @@ function imageLoadDirective () {
 
     link: function(scope, element) {
       element.on('load', function() {
+        element.next('div').removeClass('loader-visible');
+        element.next('div').children().removeClass('loader-visible');
+        element.next('div').addClass('loader-hidden');
+        element.next('div').children().addClass('loader-hidden');
         element.removeClass('spinner-hide');
-        element.addClass('spinner-show');
-        element.parent().find('span').remove();
       });
       scope.$watch('ngSrc', function() {
         element.addClass('spinner-hide');
+        element.next('div').addClass('loader-visible');
+        element.next('div').children().addClass('loader-visible');
       });
     }
   };
