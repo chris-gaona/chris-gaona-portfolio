5df4e47d0636eda0a0d6c621aa42b9ed672945fb
Chris Gaona <thebeloved88@sbcglobal.net>
2016-09-12 21:12:41 -0700

    Gets angular-ui-router initially working in app

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 // requires angular
 var angular = require('angular');
-require('angular-route');
+require('angular-ui-router');
 require('angular-scroll');
 require('angular-animate');
 require('angular-toastr');
@@ -12,22 +12,18 @@ require('ng-file-upload');
 require('chart.js');
 require('angular-svg-round-progressbar');
 
-// creates the angular app and lists dependencies
-var ngModule = angular.module('app', ['ngRoute', 'duScroll', 'ngAnimate', 'toastr', '720kb.datepicker', 'ngMap', 'ngFileUpload', 'chart.js', 'ui.calendar', 'angular-svg-round-progressbar'])
-.run(function($rootScope, $location, AuthService) {
-  // wire up the route change start handler
-  // in order to determine if the requested route requires a user login
-  $rootScope.$on('$routeChangeStart', function(event, next) {
-    // if the "require login" property is set to "true"
-    // and we don't have an authenticated user...
-    // then send the user to the "Sign In" view.
-    if (next.requireLogin && !AuthService.isLoggedIn()) {
-      $location.path('/login');
-      event.preventDefault();
-    }
-  });
-// creates config for toastr
-}).config(function(toastrConfig) {
+var ngModule = angular.module('app', [
+  'ui.router',
+  'duScroll',
+  'ngAnimate',
+  'toastr',
+  '720kb.datepicker',
+  'ngMap',
+  'ngFileUpload',
+  'chart.js',
+  'ui.calendar',
+  'angular-svg-round-progressbar'
+]).config(function(toastrConfig) {
   angular.extend(toastrConfig, {
     containerId: 'toast-container',
     positionClass: 'toast-bottom-right',
@@ -37,6 +33,32 @@ var ngModule = angular.module('app', ['ngRoute', 'duScroll', 'ngAnimate', 'toast
     preventOpenDuplicates: true
   });
 });
+
+// // creates the angular app and lists dependencies
+// var ngModule = angular.module('app', ['ngRoute', 'duScroll', 'ngAnimate', 'toastr', '720kb.datepicker', 'ngMap', 'ngFileUpload', 'chart.js', 'ui.calendar', 'angular-svg-round-progressbar'])
+// .run(function($rootScope, $location, AuthService) {
+//   // wire up the route change start handler
+//   // in order to determine if the requested route requires a user login
+//   $rootScope.$on('$routeChangeStart', function(event, next) {
+//     // if the "require login" property is set to "true"
+//     // and we don't have an authenticated user...
+//     // then send the user to the "Sign In" view.
+//     if (next.requireLogin && !AuthService.isLoggedIn()) {
+//       $location.path('/login');
+//       event.preventDefault();
+//     }
+//   });
+// // creates config for toastr
+// }).config(function(toastrConfig) {
+//   angular.extend(toastrConfig, {
+//     containerId: 'toast-container',
+//     positionClass: 'toast-bottom-right',
+//     closeButton: true,
+//     progressBar: true,
+//     tapToDismiss: true,
+//     preventOpenDuplicates: true
+//   });
+// });
 
 // requires all needed angular files
 require('./scripts/config/route-config.js')(ngModule);
--- app/scripts/config/route-config.js
+++ app/scripts/config/route-config.js
@@ -1,77 +1,125 @@
 'use strict';
 
-function config ($routeProvider, $locationProvider) {
-  $routeProvider
-  .when('/', {
-    controller: 'MainController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/main.html',
-    resolve: {
-      projects: ['MainService', function (MainService) {
-        return MainService.getAll();
-      }],
-      treehouse: ['MainService', function (MainService) {
-        return MainService.getTreehouse();
-      }],
-      codeschool: ['MainService', function (MainService) {
-        return MainService.getCodeschool();
-      }],
-      github: ['MainService', function (MainService) {
-        return MainService.getGithub();
-      }],
-      weather: ['WeatherService', function (WeatherService) {
-        return WeatherService.getWeather();
-      }]
-    }
-  })
-  .when('/new', {
-    controller: 'ProjectController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/new-form.html',
-    requireLogin: true
-  })
-  .when('/edit/:id', {
-    controller: 'ProjectController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/new-form.html',
-    requireLogin: true
-  })
-  .when('/register', {
-    controller: 'AuthController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/authenticate.html'
-  })
-  .when('/login', {
-    controller: 'AuthController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/authenticate.html',
-    resolve: {
-    check: ['$location', 'AuthService', function($location, AuthService) {
-        if (AuthService.isLoggedIn()) {
-          $location.path('/');
+function config ($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      views: {
+        'navbar': {
+          templateUrl: 'templates/navbar.html',
+          controller: '',
+        },
+        'footer': {
+          templateUrl: 'templates/footer.html',
+          controller: ''
         }
-      }]
-    }
-  })
-  .when('/resume', {
-    controller: 'ResumeController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/resume.html'
-  })
-  .when('/dashboard', {
-    controller: 'DashboardController',
-    controllerAs: 'vm',
-    templateUrl: 'templates/dashboard.html'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+      }
+    })
+    .state('home', {
+      parent: 'root',
+      url: '/',
+      views: {
+        'container@': {
+          templateUrl: 'templates/main.html',
+          controller: 'MainController',
+          controllerAs: 'vm'
+        }
+      },
+      resolve: {
+        projects: ['MainService', function (MainService) {
+          return MainService.getAll();
+        }],
+        treehouse: ['MainService', function (MainService) {
+          return MainService.getTreehouse();
+        }],
+        codeschool: ['MainService', function (MainService) {
+          return MainService.getCodeschool();
+        }],
+        github: ['MainService', function (MainService) {
+          return MainService.getGithub();
+        }],
+        weather: ['WeatherService', function (WeatherService) {
+          return WeatherService.getWeather();
+        }]
+      }
+    });
 
-  $locationProvider
-  .html5Mode(true);
-}
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  }
+
+//   $routeProvider
+//   .when('/', {
+//     controller: 'MainController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/main.html',
+//     resolve: {
+//       projects: ['MainService', function (MainService) {
+//         return MainService.getAll();
+//       }],
+//       treehouse: ['MainService', function (MainService) {
+//         return MainService.getTreehouse();
+//       }],
+//       codeschool: ['MainService', function (MainService) {
+//         return MainService.getCodeschool();
+//       }],
+//       github: ['MainService', function (MainService) {
+//         return MainService.getGithub();
+//       }],
+//       weather: ['WeatherService', function (WeatherService) {
+//         return WeatherService.getWeather();
+//       }]
+//     }
+//   })
+//   .when('/new', {
+//     controller: 'ProjectController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/new-form.html',
+//     requireLogin: true
+//   })
+//   .when('/edit/:id', {
+//     controller: 'ProjectController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/new-form.html',
+//     requireLogin: true
+//   })
+//   .when('/register', {
+//     controller: 'AuthController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/authenticate.html'
+//   })
+//   .when('/login', {
+//     controller: 'AuthController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/authenticate.html',
+//     resolve: {
+//     check: ['$location', 'AuthService', function($location, AuthService) {
+//         if (AuthService.isLoggedIn()) {
+//           $location.path('/');
+//         }
+//       }]
+//     }
+//   })
+//   .when('/resume', {
+//     controller: 'ResumeController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/resume.html'
+//   })
+//   .when('/dashboard', {
+//     controller: 'DashboardController',
+//     controllerAs: 'vm',
+//     templateUrl: 'templates/dashboard.html'
+//   })
+//   .otherwise({
+//     redirectTo: '/'
+//   });
+//
+//   $locationProvider
+//   .html5Mode(true);
+// }
 
 module.exports = function(ngModule) {
   // creates config routes for angular application
-  ngModule.config(config);
+  ngModule.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', config]);
 };
